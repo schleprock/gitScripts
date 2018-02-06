535eda4f19d306c398b5551cf1069864c1dc7189
William J. Schilp, PhD <william.schilp@ansys.com>
2018-02-06 10:57:15 -0500

    highlite branches that are not on develop

--- gitTest.pl
+++ gitTest.pl
@@ -4,7 +4,9 @@ use strict;
 use GitModules;
 use Cwd;
 use File::Basename;
+use Term::ANSIColor;
 
 my $pwd = getcwd();
 my $base = basename($pwd);
+print color 'bold';
 print("\npwd = $pwd; base = $base\n\n");
